3908cd581d3ae035dd874165ca84cfbec353bde7
kimukkimu <23468175+kimukkimu@users.noreply.github.com>
2024-11-02 20:24:32 +0900

    7章まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,18 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
-
+import Pagination from "@/app/_components/Pagination";
+import { NEWS_LIST_LIMIT } from "../_constants";
+import SearchField from "../_components/SearchField";
 export default async function Page() {
-  const { contents: news } = await getNewsList();
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+  });
 
-  return <NewsList news={news} />;
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
+    </>
+  )
 }
